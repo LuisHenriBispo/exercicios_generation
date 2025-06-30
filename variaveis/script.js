bcbf49cf0a6d45c447f171f67531e592a0f9b23c
Luis Bispo <duimbispo@gmail.com>
2025-06-30 15:33:32 -0300

    Codigos da aula de hoje

---
+++ variaveis/script.js
@@ -0,0 +1,20 @@
+//let nome ="Luis";
+let idade = 20;
+const planeta = "Terra";
+
+// console.log(nome);
+// console.log(idade);
+// console.log(planeta);
+
+let humor = "Feliz";
+humor ="com fome";
+
+console.log(humor);
+
+const fav = "Azul";
+
+//Saida de dados
+let nome ="Luis";
+console.log("ola" + nome + "!");
+
+console.log(`ola, ${nome}!`);
